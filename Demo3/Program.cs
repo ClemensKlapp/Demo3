fd7a652e022cba5db691f8b5acebb946a5b49af1
Clemens Klapp <clemens.klapp@gmail.com>
2024-12-05 09:54:48 +0100

    Update Program.cs

--- Demo3/Program.cs
+++ Demo3/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("Changed in Github");
             Console.WriteLine("Changed in branch github-branch");
             Console.WriteLine("Changed local");
+            Console.WriteLine("Changed once again in branch github-branch");
         }
     }
 }
